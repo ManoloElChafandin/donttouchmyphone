9166f90de0e00603e3779232d447397713995e49
ManuelMaganaMarquez <manuelmaganamarquez@gmail.com>
2024-10-03 10:59:13 +0200

    comepito

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace VirtualPetGame;
+
+public class Pet
+{
+    public long UniqueID { get; private set; }
+    public float Health { get; set; }
+    public float Age { get; set; }
+    public string Name { get; set; }
+    public decimal Money { get; set; }
+    public float Happiness { get; set; }
+
+    public Pet(string name)
+    {
+        Name = name;
+    }
+
+    public Pet(string name, float age, float health, float happiness)
+    {
+        Name = name;
+        Age = age;
+        Health = health;
+        Happiness = happiness;
+
+    }
+    public void Eat(float amount)
+    {
+        Console.WriteLine(Name + " ate " + amount + " food!");
+    }
+
+    public void Gamble(int duration)
+    {
+        Console.WriteLine(Name + " gambled for " + duration + " minutes.");
+    }
+
+    public void Play(int duration)
+    {
+        Console.WriteLine(Name + " played for " + duration + " minutes.");
+    }
+}
+
+
+
+public class VirtualPetManager
+{
+    public static void Main()
+    {
+        Pet pet = new("Niggi", 2, 100.0f, 10.0f);
+        pet.Eat(50);
+        pet.Gamble(30);
+        Console.WriteLine(pet.Name + " happiness is " + pet.Happiness);
+    }
+}
---
+++ puterrobot/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ROBOTBETIS;
+
+public class Robot
+{
+    public string Model{ get; set; }
+    private string Manufacturer { get; set; }
+    public float BatteryLevel { get; set; }
+    public float Speed { get; set;}
+
+    public Move()
+    {
+        Console.WriteLine(Model + " moves at " + speed + " km/h!");
+    }
+
+    void class Main()
+    {
+
+    }
+    public class Robot (string model = "Puterrobot", private string manufacturer = "Chinese Temu Factory Workers", float batteryLevel = 100.00f, float speed = 15.00f)
+    {
+        Model= model;
+        Manufacturer = manufacturer;
+        BatteryLevel = batterylevel;
+        Speed = speed;
+    }
+
+}
